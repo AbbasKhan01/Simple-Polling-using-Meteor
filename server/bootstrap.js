d6b21d4373a82debca9af43d48149cc20be5045e
Abbas Khan <itsabbas01@gmail.com>
2016-06-22 20:52:35 +0200

    Add files via upload

---
+++ server/bootstrap.js
@@ -0,0 +1,34 @@
+// run this when the meteor app is started
+Meteor.startup(function() {
+
+  // if there are no polls available create sample data
+  if (Polls.find().count() === 0) {
+
+    // create sample polls
+    var samplePolls = [
+      {
+        question: 'Is this an awesome app?',
+        choices: [
+          { text: 'Of course!', votes: 0 },
+          { text: 'Eh', votes: 0 },
+          { text: 'No. I dont like it', votes: 0 }
+        ]
+      },
+      {
+        question: 'Is meteor awesome?',
+        choices: [
+          { text: '100% yes', votes: 0 },
+          { text: '200% yes', votes: 0 },
+          { text: '300% yes', votes: 0 }
+        ]
+      }
+    ];
+
+    // loop over each sample poll and insert into database
+    _.each(samplePolls, function(poll) {
+      Polls.insert(poll);
+    });
+
+  }
+
+});
